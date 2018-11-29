cb345099fa5b98b9296617e0def517e665f21635
Daniel Moreira <danieldenis01@gmail.com>
2018-11-29 20:33:30 -0200

    feat(hipercard): Adiciona suporte ao Hipercard

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,16 +4,24 @@
       <div class="jp-card" :class="classCard">
         <div class="jp-card-front">
           <div class="jp-card-logo jp-card-elo">
-          <div class="e">e</div>
-          <div class="l">l</div>
-          <div class="o">o</div>
+            <div class="e">e</div>
+            <div class="l">l</div>
+            <div class="o">o</div>
           </div>
-          <div class="jp-card-logo jp-card-visa">visa</div>
-          <div class="jp-card-logo jp-card-mastercard">MasterCard</div>
+          <div class="jp-card-logo jp-card-visa">Visa</div>
+          <div class="jp-card-logo jp-card-visaelectron">Visa<div class="elec">Electron</div></div>
+          <div class="jp-card-logo jp-card-mastercard">Mastercard</div>
           <div class="jp-card-logo jp-card-maestro">Maestro</div>
           <div class="jp-card-logo jp-card-amex"></div>
           <div class="jp-card-logo jp-card-discover">discover</div>
+          <div class="jp-card-logo jp-card-dinersclub"></div>
+          <div class="jp-card-logo jp-card-hipercard">Hipercard</div>
           <div class="jp-card-logo jp-card-dankort"><div class="dk"><div class="d"></div><div class="k"></div></div></div>
+          <div class="jp-card-logo jp-card-jcb">
+            <div class="j">J</div>
+            <div class="c">C</div>
+            <div class="b">B</div>
+          </div>
           <div class="jp-card-lower">
             <div class="jp-card-shiny"></div>
             <div class="jp-card-cvc jp-card-display" :class="classDisplay['cvc']">{{ display.cvc }}</div>
@@ -53,7 +61,8 @@ let options = {
     'unionpay',
     'visa',
     'visaelectron',
-    'elo'
+    'elo',
+    'hipercard'
   ],
   inputTypes: [
     'number',
